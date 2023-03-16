3b4b54379028a5eaf274552a28c690ba4fe062a8
zu3x <zu3x@fresnou.org>
2023-03-16 11:32:59 -0700

    Second commit, slide 56, adding permission logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,12 @@
-const {User, Student} = require('../models')
+const {User, Student, Staff} = require('../models')
 const md5 = require('md5')
 const passport = require('passport');
 
 module.exports.renderStudentRegistrationForm = function(req,res){
-    res.render('user/register');
+    res.render('user/registerStudent');
+}
+module.exports.renderStaffRegistrationForm = function(req,res){
+    res.render('user/registerStaff');
 }
 module.exports.registerStudent = async function(req,res){
     const user = await User.create({
@@ -15,6 +18,19 @@ module.exports.registerStudent = async function(req,res){
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         grade_level: req.body.grade_level,
+        user_id: user.id
+    });
+    res.redirect('/courses')
+}
+module.exports.registerStaff = async function(req,res){
+    const user = await User.create({
+        email: req.body.email,
+        password:md5(req.body.password),
+        role: 'staff'
+    });
+    await Staff.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
         user_id: user.id
     });
     res.redirect('/courses')
@@ -31,3 +47,7 @@ module.exports.login = passport.authenticate('local', {
     failureRedirect: '/login',
     failureMessage: true
 });
+module.exports.logout = function(req, res){
+    req.logout();
+    res.redirect('/login');
+}
--- routes/index.js
+++ routes/index.js
@@ -3,37 +3,46 @@ var router = express.Router();
 const courseController = require('../controllers/courseController.js');
 const studentController = require('../controllers/studentController.js');
 const userController = require('../controllers/userController.js');
-
+function redirectGuest(req,res,next){
+  if (!req.user){
+    res.redirect('/login');
+    return
+  }
+  next();
+};
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/courses');
 });
 
-router.get('/courses', courseController.viewAll);
-router.get('/courses/profile/:id', courseController.viewProfile);
-router.get('/courses/edit/:id', courseController.renderEditForm);
-router.post('/courses/edit/:id', courseController.updateCourse);
-router.get('/courses/add', courseController.renderAddForm);
-router.post('/courses/add', courseController.addCourse);
-router.get('/courses/delete/:id', courseController.deleteCourse);
+router.get('/courses', redirectGuest, courseController.viewAll);
+router.get('/courses/profile/:id', redirectGuest, courseController.viewProfile);
+router.get('/courses/edit/:id', redirectGuest, courseController.renderEditForm);
+router.post('/courses/edit/:id', redirectGuest, courseController.updateCourse);
+router.get('/courses/add', redirectGuest, courseController.renderAddForm);
+router.post('/courses/add', redirectGuest, courseController.addCourse);
+router.get('/courses/delete/:id', redirectGuest, courseController.deleteCourse);
 
 
-router.get('/students', studentController.viewAll);
-router.get('/students/profile/:id', studentController.viewProfile);
-router.get('/students/edit/:id', studentController.renderEditForm);
-router.post('/students/edit/:id', studentController.updateStudent);
-router.get('/students/add', studentController.renderAddForm);
-router.post('/students/add', studentController.addStudent);
-router.get('/students/delete/:id', studentController.deleteStudent);
+router.get('/students', redirectGuest, studentController.viewAll);
+router.get('/students/profile/:id', redirectGuest, studentController.viewProfile);
+router.get('/students/edit/:id', redirectGuest, studentController.renderEditForm);
+router.post('/students/edit/:id', redirectGuest, studentController.updateStudent);
+router.get('/students/add', redirectGuest, studentController.renderAddForm);
+router.post('/students/add', redirectGuest, studentController.addStudent);
+router.get('/students/delete/:id', redirectGuest, studentController.deleteStudent);
 
-router.post('/students/:studentId/enroll/', studentController.enrollStudent);
-router.get('/students/:studentId/removeCourse/:courseId', studentController.removeCourse);
-router.post('/courses/:courseId/enroll', courseController.enrollStudent);
-router.get('/courses/:courseId/removeStudent/:studentId', courseController.removeStudent);
+router.post('/students/:studentId/enroll/', redirectGuest, studentController.enrollStudent);
+router.get('/students/:studentId/removeCourse/:courseId', redirectGuest, studentController.removeCourse);
+router.post('/courses/:courseId/enroll', redirectGuest, courseController.enrollStudent);
+router.get('/courses/:courseId/removeStudent/:studentId', redirectGuest, courseController.removeStudent);
 
 router.get('/register-student', userController.renderStudentRegistrationForm);
 router.post('/register-student', userController.registerStudent);
+router.get('/register-staff', userController.renderStaffRegistrationForm);
+router.post('/register-staff', userController.registerStaff);
 router.get('/login', userController.renderLoginForm);
 router.post('/login', userController.login);
 
+router.get('/logout', userController.logout);
 module.exports = router;
